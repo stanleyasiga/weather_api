daca67130d75ee7452720f48ca8e51eecd7e5a18
Stanley <austinstan24@gmail.com>
2025-06-27 01:31:39 +0300

    fix: added weather api data

--- src/server.js
+++ src/server.js
@@ -1,41 +1,14 @@
-const http = require("http");
-
-const PORT = process.env.port || 3000;
-
 const axios = require("axios");
 
-const sendJsonResponse = (res, statusCode, data) => {
-  res.writeHead(statusCode, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(data));
-};
-
-const sendErrorResponse = (res, statusCode, message) => {
-  res.writeHead(statusCode, { "Content-Type": "text/plain" });
-  res.end(message);
-};
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/api/weather" && method === "GET") {
-    sendJsonResponse(res, "200", {
-      status: "success",
-      results: products.length,
-      data: {},
-    });
-  } else {
-    sendErrorResponse(res, "404", "the api endpoint does not exist");
+const getWeatherDataApi = async () => {
+  try {
+    const response = await axios.get(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/kampala?key=BY6KU2J88B3YDGFWWRULG678Z`
+    );
+    console.log(response?.data);
+  } catch (error) {
+    console.log(error);
   }
-});
+};
 
-server.listen(PORT, () => {
-  console.log(``);
-});
-
-// axios.get(
-//   `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`,
-//   {}
-// );
-
-const weatherResponse = () => {};
+getWeatherDataApi();
